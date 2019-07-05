f730d90b53a79863c8cea90ab0e59a4310118039
Eliza Munroe <era3munroe@gmail.com>
2019-07-05 12:08:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -99,7 +99,7 @@ def adding_matz
      }
 
 programmer_hash
-        :yukihiro_matsumoto => {
+        :yukihiro_matsumoto = {
             :known_for => "Ruby", 
             :languages => ["LISP", "C"]
   
