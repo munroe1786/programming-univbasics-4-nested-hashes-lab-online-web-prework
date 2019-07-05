7c9b1a61fbc6eebbc044d5c248c26e7701670e6a
Eliza Munroe <era3munroe@gmail.com>
2019-07-05 11:56:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,7 +68,7 @@ def dennis_ritchies_language
      
 end
 
-programmer_hash[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages] = "C"
 
 return programmer_hash[:dennis_ritchie][:languages]
 
